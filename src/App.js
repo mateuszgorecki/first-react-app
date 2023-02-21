8b2d6323cb7cfc12b6ab1c7d727f7d57982cf7f0
mateusz <mateusz.gorecki.dev@gmail.com>
2023-02-21 20:48:02 +0100

    change animation of switching pages and add dynamically page title

--- src/App.js
+++ src/App.js
@@ -9,20 +9,39 @@ import ContactPage from './pages/Contact'
 import HomePage from './pages/Home'
 import PortfolioPage from './pages/Portfolio'
 import PageDetails from './components/PageDetails'
-// import Footer from './components/layout/Footer'
 
 import './assets/scss/_global.scss'
 import ThemeContext from './store/theme-context'
+import { useEffect } from 'react'
 
 function App() {
   const location = useLocation()
+  const pageTitle = location.pathname
 
   const ctx = useContext(ThemeContext)
   const isDark = ctx.isDarkTheme
 
   const body = document.querySelector('body')
-
   isDark ? body.classList.add('dark') : body.classList.remove('dark')
+
+  useEffect(() => {
+    let title
+    if (pageTitle !== '/') {
+      title = pageTitle.charAt(1).toUpperCase() + pageTitle.slice(2)
+
+      if (title.indexOf('/') > 0) {
+        title =
+          title.substring(0, 9) +
+          ' - ' +
+          title.slice(10).charAt(0).toUpperCase() +
+          title.slice(11)
+      }
+    } else if (pageTitle === '/') {
+      title = 'Home'
+    }
+
+    document.title = title
+  }, [pageTitle])
 
   return (
     <div>
@@ -34,19 +53,18 @@ function App() {
             key={location.pathname}
           >
             <Route
-            index
+              index
               path='/'
               element={<HomePage />}
             />
             <Route
               path='portfolio'
               element={<PortfolioPage />}
-            >
-            </Route>
-              <Route
-                path='portfolio/:pageID'
-                element={<PageDetails />}
-              />
+            ></Route>
+            <Route
+              path='portfolio/:pageID'
+              element={<PageDetails />}
+            />
             <Route
               path='about'
               element={<AboutPage />}
--- src/components/layout/PageWrapper.js
+++ src/components/layout/PageWrapper.js
@@ -13,18 +13,23 @@ const PageWrapper = (props) => {
   return (
     <motion.div
       initial={{
-        opacity: 0,
-        y: 50,
-      }}
-      animate={{
-        opacity: 1,
-        y: 0,
-      }}
-      exit={{
-        opacity: 0,
-        y: 0,
-        transition: { duration: 0.5 },
-      }}
+                opacity: 0,
+                scale: 0.9,
+              }}
+              animate={{
+                opacity: 1,
+                scale: 1,
+              }}
+              exit={{
+                opacity: 0,
+                scale: 0.9,
+                transition: {
+                  duration: 0.2
+                }
+              }}
+              transition={{
+                duration: 0.4,
+              }}
     >
       <div
         className={` ${classes.wrapper} ${themeClasses} ${props.className}`}
